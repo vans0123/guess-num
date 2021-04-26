e8a9da2a7bce72d6dd1f61956f39cd0c5df07e11
vans0123 <79493802+vans0123@users.noreply.github.com>
2021-04-26 21:21:39 +0800

    Add files via upload

---
+++ guess-num.py
@@ -0,0 +1,18 @@
+import random
+start = int(input('請使用者輸入開始值:'))
+end = int(input('請使用者輸入結束值:'))
+r = random.randint(start,end)
+count = 0
+while true:
+   count = count + 1
+   num = input('請輸入數字')
+   num = int(num)
+   if num == r:
+      print('你答對了')
+      break
+   elif num > r:
+      print('比答案大')
+   elif num < r:
+      print('比答案小')
+   print('這是你猜的第幾次', count,'次')
+
